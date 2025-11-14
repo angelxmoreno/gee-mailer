5d758747581335b0623fbfbad92977c86bfb2d1e
Angel S. Moreno <angelxmoreno@gmail.com>
2025-11-14 13:56:24 -0500

    chore: created config builder

---
+++ src/types/AppConfig.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+import { LogLevel } from './LogLevel.ts';
+import { NodeEnv } from './NodeEnv.ts';
+
+export const AppConfigSchema = z.object({
+    nodeEnv: z.object({
+        env: z.enum([NodeEnv.development, NodeEnv.test, NodeEnv.production]),
+        isDevelopment: z.boolean(),
+        isTesting: z.boolean(),
+    }),
+    logger: z.object({
+        usePretty: z.boolean().optional().default(true),
+        level: z.enum(LogLevel).optional().default(LogLevel.info),
+    }),
+});
+
+export type AppConfig = z.infer<typeof AppConfigSchema>;
